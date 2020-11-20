42df756ff98b96a0f8849094afdbb47aac53a7c9
eddily <72766373+eddily@users.noreply.github.com>
2020-11-19 19:59:49 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 F = input.temperature(TemperatureUnit.FAHRENHEIT)
-if F > 60
-    
+while True:
+    print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT))
+    if F > 70:
+        light.set_pixel_color(5, light.rgb(255, 0, 0))
+    elif F > 40:
+        light.set_pixel_color(5, light.rgb(0, 255, 0))
+    else :
+        light.set_pixel_color(5, light.rgb(0, 0, 255))
+        
+
--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
 let F = input.temperature(TemperatureUnit.Fahrenheit)
+while (true) {
+    console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit))
+    if (F > 70) {
+        light.setPixelColor(5, light.rgb(255, 0, 0))
+    } else if (F > 40) {
+        light.setPixelColor(5, light.rgb(0, 255, 0))
+    } else {
+        light.setPixelColor(5, light.rgb(0, 0, 255))
+    }
+    
+}
